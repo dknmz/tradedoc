8aa402f496890751053efb2131fa30eda0eee8b9
mroach <git@c.mroach.com>
2026-08-18 10:45:08 +0200

    feat(xml): detect invalid node names

--- lib/tradedoc/xml/writer.rb
+++ lib/tradedoc/xml/writer.rb
@@ -3,6 +3,8 @@ module Tradedoc
     # Wrapper for a `Nokogiri::XML::Builder` that creates an interface for common
     # build actions, namely rendering "components" easily.
     class Writer
+      InvalidNodeNameError = Class.new(Error)
+
       # @param xml [Nokogiri::XML::Builder]
       # @param format [Module]
       #   Any module responding to `.coder_for` and returning a coder.
@@ -16,6 +18,14 @@ module Tradedoc
       # Normally with a Nokogiri builder you'd do `xml["ns"].ElemName(text)`
       # but with this you'd do `writer.add("ns:ElemName", value)`
       def add(name, ...)
+        if name.include?("/")
+          raise InvalidNodeNameError, "element name cannot include `/`"
+        end
+
+        if (c = name.scan(":").count) && c > 1
+          raise InvalidNodeNameError, "element name can't include `:` multiple times"
+        end
+
         xml.public_send(name, ...)
       end
 
